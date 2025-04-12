055f6897ab5097889267f7977498f2ffc5cb2a09
Irsyad Nafi <irsyadnap33@gmail.com>
2025-04-12 14:22:33 +0700

    setup server done

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,17 @@
 import express from "express";
 import { configDotenv } from "dotenv";
+import userRoutes from "./routes/user.routes.js";
 
 configDotenv();
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+// Routing
+app.use(userRoutes);
+
+export default app;
