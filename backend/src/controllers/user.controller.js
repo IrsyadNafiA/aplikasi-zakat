99fbef0c422c9828b1842898e6441d56e1ec37c3
Irsyad Nafi <irsyadnap33@gmail.com>
2025-04-12 16:56:14 +0700

    setup prisma and create password utils

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,7 +1,12 @@
-const getUsers = (req, res) => {
-  res.status(200).json({
-    message: "Get user",
-  });
+import prisma from "../config/prisma.js";
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export { getUsers };
---
+++ backend/src/utils/password.utils.js
@@ -0,0 +1,14 @@
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
+
+export const hashPassword = (password) => {
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(password, salt);
+
+  return hash;
+};
+
+export const comparePassword = (plain, hashed) => {
+  return bcrypt.compareSync(plain, hashed);
+};
