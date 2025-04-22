2c1e881fcb3a564b244cd99f3ab493d35882138f
Irsyad Nafi <121683533+IrsyadNafiA@users.noreply.github.com>
2025-04-22 14:36:40 +0700

    create(frontend): setup frontend folder and install router & mui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import { createBrowserRouter } from "react-router";
+
+const App = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Home</h1>,
+  },
+]);
+
+export default App;
